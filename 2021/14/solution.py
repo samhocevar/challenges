34f4c6ca68fe99c22829c8d7b9163aa562d7eb95
Sam Hocevar <sam@hocevar.net>
2022-12-06 20:37:57 +0100

    Add a few comments

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -10,26 +10,28 @@ with open('input.txt') as f:
         rules[a] = [a[0] + b, b + a[1]]
         letters.add(b)
 
-# Initialize state with chain
-state = { k: 0 for k in rules }
+# Initialize stats with chain
+stats = { k: 0 for k in rules }
 for x, y in zip(chain, chain[1:]):
-    state[x + y] += 1
+    stats[x + y] += 1
 
 # Apply rules to the stats instead of the actual chain
 def step():
     todo = { k: 0 for k in rules }
     for k, v in rules.items():
-        todo[k] -= state[k]
-        todo[v[0]] += state[k]
-        todo[v[1]] += state[k]
+        todo[k] -= stats[k]
+        todo[v[0]] += stats[k]
+        todo[v[1]] += stats[k]
     for k, v in todo.items():
-        state[k] += v
+        stats[k] += v
 
+# Don’t forget to count the last character of the chain! It’s not in the stats
 def count(l):
-    return sum(v for k, v in state.items() if k[0] == l) + (chain[-1] == l)
+    return sum(v for k, v in stats.items() if k[0] == l) + (chain[-1] == l)
 
 for x in range(40):
     step()
     if x + 1 in [10, 40]:
-        print(max(map(count, letters)) - min(map(count, letters)))
+        counts = list(map(count, letters))
+        print(max(counts) - min(counts))
 
