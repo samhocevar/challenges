704353609252172396b59a2d7f1e56e4f4b9bb55
Sam Hocevar <sam@hocevar.net>
2022-12-07 10:10:47 +0100

    Better Python code for December 7th, 2022

--- 2022/07/solution.py
+++ 2022/07/solution.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-from itertools import accumulate
-
 pwd = ()
 sizes = {(): 0}
 
@@ -14,20 +12,16 @@ with open('input.txt') as f:
             elif directory == '..':
                 pwd = pwd[:-1]
             elif directory:
-                pwd = (*pwd, directory)
+                pwd += (directory,)
+        elif line[0] == 'd':
+            sizes.setdefault(pwd + (line.split()[1],), 0)
         else:
-            if line[0] == 'd':
-                path = (*pwd, line.split()[1])
-                if path in sizes:
-                    print('OOF')
-                sizes[path] = 0
-            else:
-                s = int(line.split()[0])
-                for path in accumulate(map(lambda d: (d,), pwd), initial=()):
-                    sizes[path] += s
+            sizes[pwd] += int(line.split()[0])
 
-print(sum(x for x in sizes.values() if x <= 100000))
+# Accumulate directory sizes using all subdirectories
+totalsizes = { dir: sum(size for subdir, size in sizes.items() if subdir[:len(dir)] == dir) for dir in sizes.keys() }
+excess = totalsizes[()] - (70000000 - 30000000)
 
-excess = sizes[()] - (70000000 - 30000000)
-print(min(x for x in sizes.values() if x >= excess))
+print(sum(x for x in totalsizes.values() if x <= 100000))
+print(min(x for x in totalsizes.values() if x >= excess))
 
