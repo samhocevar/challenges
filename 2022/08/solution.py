d336a6a44d0b5244634d91e0697c9d9d906166f3
Sam Hocevar <sam@hocevar.net>
2022-12-08 08:28:30 +0100

    December 8th, 2022 (Python)

---
+++ 2022/08/solution.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+with open('input.txt') as f:
+    data = [[int(x) for x in l.strip()] for l in f]
+
+grid = np.array(data)
+top = 1 + np.amax(grid)
+visibility = np.full(grid.shape, False)
+score = np.ones(grid.shape).astype(int)
+
+for _ in range(4):
+    for y in range(grid.shape[0]):
+        hmax = -1
+        last = [0] * top
+        for x in range(grid.shape[1]):
+            h = grid[(y,x)]
+            # Visibility: true if the tree is higher than the highest encountered tree so far.
+            if h > hmax:
+                visibility[(y,x)] = True
+                hmax = h
+            # Scenic score: distance to the last tree that was as high as the current one.
+            score[(y,x)] *= x - last[h]
+            last = [x] * (h + 1) + last[h+1:]
+    # Rotate everything 90° and start again
+    grid, visibility, score = map(np.rot90, (grid, visibility, score))
+
+print(len(np.nonzero(visibility)[0]))
+print(np.amax(score))
+
