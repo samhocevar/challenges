8c5514a31dabe836f76b3b73b725e0c43ad35b84
Sam Hocevar <sam@hocevar.net>
2022-12-10 07:04:02 +0100

    December 10th, 2022 (Python)

---
+++ 2022/10/solution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+signal, strength, crt = 1, 0, ''
+
+def parse(f):
+    for l in f:
+        match l.split():
+            case 'addx', x: yield 0; yield int(x)
+            case _: yield 0
+
+with open('input.txt') as f:
+    for cycle, x in enumerate(parse(f), start=1):
+        if (cycle + 20) % 40 == 0:
+            strength += signal * cycle
+        crt += '#' if abs(signal - (cycle - 1) % 40) <= 1 else ' '
+        signal += x
+
+print(strength)
+
+for s in range(0, 40 * 6, 40):
+    print(crt[s:s+40])
