0d0f382e627d5caa0db1d8535779d64d2e18f705
Sam Hocevar <sam@hocevar.net>
2022-12-13 10:21:37 +0100

    Use ast module for parsing instead of json

--- 2022/13/solution.py
+++ 2022/13/solution.py
@@ -2,7 +2,7 @@
 
 from functools import cmp_to_key
 from math import prod
-from json import loads
+from ast import literal_eval
 
 mark = [[[2]], [[6]]]
 
@@ -14,7 +14,7 @@ def cmp(l, r):
     return next((c for c in map(cmp, l, r) if c), len(l) - len(r))
 
 with open('input.txt') as f:
-    pkts = list(map(loads, [l for l in f if l.strip()]))
+    pkts = list(map(literal_eval, [l for l in f if l.strip()]))
 
 print(sum(i for i, p in enumerate(zip(pkts[::2], pkts[1::2]), 1) if cmp(*p) <= 0))
 print(prod(i for i, p in enumerate(sorted(pkts + mark, key=cmp_to_key(cmp)), 1) if p in mark))
