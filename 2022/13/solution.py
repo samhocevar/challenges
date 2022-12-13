948c864c7a590da7b1693647c6d1824c7ac13758
Sam Hocevar <sam@hocevar.net>
2022-12-13 11:41:25 +0100

    Just count instead of sorting…

--- 2022/13/solution.py
+++ 2022/13/solution.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 
-from functools import cmp_to_key
-from math import prod
 from ast import literal_eval
-
-mark = [[[2]], [[6]]]
 
 def cmp(l, r):
     match l, r:
@@ -16,5 +12,5 @@ def cmp(l, r):
 with open('input.txt') as f:
     pkts = list(map(literal_eval, [l for l in f if l.strip()]))
 
-print(sum(i for i, p in enumerate(zip(pkts[::2], pkts[1::2]), 1) if cmp(*p) <= 0))
-print(prod(i for i, p in enumerate(sorted(pkts + mark, key=cmp_to_key(cmp)), 1) if p in mark))
+print(sum(i for i, p in enumerate(zip(*[iter(pkts)] * 2), 1) if cmp(*p) <= 0))
+print(sum((1 for p in pkts if cmp(p, [[2]]) < 0), 1) * sum((1 for p in pkts if cmp(p, [[6]]) < 0), 2))
