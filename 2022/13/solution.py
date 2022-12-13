c2ed0d65f26d10f0e78803de04035374faec4662
Sam Hocevar <sam@hocevar.net>
2022-12-13 12:18:34 +0100

    Make changes suggested on Reddit

--- 2022/13/solution.py
+++ 2022/13/solution.py
@@ -7,10 +7,10 @@ def cmp(l, r):
         case int(), int(): return l - r
         case list(), int(): r = [r]
         case int(), list(): l = [l]
-    return next((c for c in map(cmp, l, r) if c), len(l) - len(r))
+    return next(filter(bool, map(cmp, l, r)), len(l) - len(r))
 
 with open('input.txt') as f:
-    pkts = list(map(literal_eval, [l for l in f if l.strip()]))
+    pkts = [literal_eval(l) for l in f if l.strip()]
 
 print(sum(i for i, p in enumerate(zip(*[iter(pkts)] * 2), 1) if cmp(*p) <= 0))
 print(sum((1 for p in pkts if cmp(p, [[2]]) < 0), 1) * sum((1 for p in pkts if cmp(p, [[6]]) < 0), 2))
