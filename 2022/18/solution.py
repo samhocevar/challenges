0eb353365605f02479b3f320eea2858ae1324aed
Sam Hocevar <sam@hocevar.net>
2022-12-18 21:28:40 +0100

    Better use of NumPy in the droplet problem

--- 2022/18/solution.py
+++ 2022/18/solution.py
@@ -4,24 +4,21 @@ from operator import add
 import numpy as np
 
 with open('input.txt') as f:
-    droplets = [list(map(int, s.split(','))) for s in map(str.strip, f)]
+    droplets = tuple(zip(*((1 + int(c) for c in l.split(',')) for l in f)))
+size = 2 + max(*sum(droplets, ()))
 
-size = 3 + max(*sum(droplets, []))
 grid = np.zeros(size * size * size).astype(int).reshape(size, size, size)
+grid[droplets] = 1
 
-for x, y, z in droplets:
-    grid[(z + 1, y + 1, x + 1)] = 1
-
-print(sum(np.count_nonzero(np.diff(grid, axis=n)) for n in range(3)))
+print(s := sum(np.count_nonzero(np.diff(grid, axis=a)) for a in range(3)))
 
 todo = [(0,0,0)]
 while todo:
     p = todo.pop()
-    grid[p] = 3
+    grid[p] = 1
     for d in [(-1,0,0), (1,0,0), (0,-1,0), (0,1,0), (0,0,-1), (0,0,1)]:
         p2 = tuple(map(add, p, d))
         if all(c >= 0 and c < size for c in p2) and grid[p2] == 0:
             todo.append(p2)
-grid = (3 - grid) // 2
 
-print(sum(np.count_nonzero(np.diff(grid, axis=n)) for n in range(3)))
+print(s - sum(np.count_nonzero(np.diff(grid, axis=a)) for a in range(3)))
