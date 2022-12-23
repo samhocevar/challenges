1e09cfc68b4cf3f93647385bca690b876a3ace9d
Sam Hocevar <sam@hocevar.net>
2022-12-23 13:37:07 +0100

    Dec 23rd, 2022: use bitmasks to make the whole thing faster

--- 2022/23/solution.py
+++ 2022/23/solution.py
@@ -5,45 +5,40 @@ from collections import defaultdict
 import numpy as np
 
 with open('input.txt') as f:
-    grid = np.array(list(["#.".index(c) for c in l] for l in map(str.strip, f)))
+    grid = np.array(list([".#".index(c) for c in l] for l in map(str.strip, f)))
+
+DIRS =  [(-1, 0), (1, 0), (0, -1), (0, 1)]
+MASKS = [    0x7,  0x1c0,    0x49,  0x124]
 
 for n in count(1):
-    # Pad grid
-    grid = np.pad(grid, 1, constant_values=1)
-    h, w = grid.shape
+    # Pad grid and compute neighbourhoods through convolution
+    #  0x1   0x2   0x4
+    #  0x8  0x10  0x20
+    #  0x40 0x80 0x100
+    grid = np.pad(grid, 1, constant_values=0)
+    neig = np.pad(grid[:,:-1], ((0, 0), (1, 0))) + (grid << 1) + (np.pad(grid[:,1:], ((0, 0), (0, 1))) << 2)
+    neig = np.pad(neig[:-1,:], ((1, 0), (0, 0))) + (neig << 3) + (np.pad(neig[1:,:], ((0, 1), (0, 0))) << 6)
 
+    # Look where each elf wants to move; only keep destinations targeted by one elf
     moves = defaultdict(list)
-    for y in range(1, h - 1):
-        for x in range(1, w - 1):
-            if grid[(y, x)]:
-                continue
-            neighbours = [[grid[(y + j, x + i)] for i in range(-1, 2)] for j in range(-1, 2)]
-            if sum(map(sum, neighbours)) == 8:
-                continue
-            for d in range(4):
-                match (d + n - 1) % 4:
-                    case 0: # North
-                        if sum(neighbours[0]) == 3: moves[(y - 1, x)].append((y, x)); break
-                    case 1: # South
-                        if sum(neighbours[2]) == 3: moves[(y + 1, x)].append((y, x)); break
-                    case 2: # West
-                        if all(neighbours[n][0] for n in range(3)): moves[(y, x - 1)].append((y, x)); break
-                    case 3: # East
-                        if all(neighbours[n][2] for n in range(3)): moves[(y, x + 1)].append((y, x)); break
+    for p in np.argwhere(np.logical_and(grid, neig & ~0x10)):
+        for d in [(k + n - 1) % 4 for k in range(4)]:
+            if neig[tuple(p)] & MASKS[d] == 0:
+                moves[(p[0] + DIRS[d][0], p[1] + DIRS[d][1])].append(p)
+                break
+    moves = {dst: tuple(src[0]) for dst, src in moves.items() if len(src) == 1}
+    for dst, src in moves.items():
+        grid[src], grid[dst] = 0, 1
+
     if not moves:
         print(n)
         break
 
-    for dst, src in moves.items():
-        if len(src) == 1:
-            grid[src[0]] = 1
-            grid[dst] = 0
-
-    # Shrink grid
-    while grid[-1,:].all(): grid = grid[:-1,:]
-    while grid[ 0,:].all(): grid = grid[1:, :]
-    while grid[:,-1].all(): grid = grid[:,:-1]
-    while grid[:, 0].all(): grid = grid[:, 1:]
+    # Shrink grid to save some memory
+    while not grid[-1,:].any(): grid = grid[:-1,:]
+    while not grid[ 0,:].any(): grid = grid[1:, :]
+    while not grid[:,-1].any(): grid = grid[:,:-1]
+    while not grid[:, 0].any(): grid = grid[:, 1:]
 
     if n == 10:
-        print(grid.sum())
+        print(grid.size - grid.sum())
