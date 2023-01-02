a289224a7e5711aa1f7fad3aa040e7079c75cc1e
Sam Hocevar <sam@hocevar.net>
2023-01-02 13:51:45 +0100

    Simplify solution and add comments

--- 2021/24/solution.py
+++ 2021/24/solution.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
-chunks = []
+abc = []
 with open('input.txt') as f:
-    for l in f:
-        match l.split():
-            case 'inp', a:
-                chunks.append([('inp', a)])
-            case op, a, b:
-                chunks[-1].append((op, a, b))
-
-# This is the really important part of the program. Each chunk contains the same code
-# except for these constants. Because of how these constants are chosen, 7 chunks cause
-# z to increase, and 7 other cause it do decrease (if the right input is given).
-abc = [tuple(map(int, (chunk[4][2], chunk[5][2], chunk[15][2]))) for chunk in chunks]
+    for n, l in enumerate(f):
+        # The program contains 14 chunks of 18 instructions. This is the really important part.
+        # Each chunk contains the same code except for these constants. Because of how they are
+        # chosen, 7 chunks cause z to increase, and 7 other cause it do decrease (provided the
+        # right input is given).
+        if n % 18 in [4, 5, 15]:
+            abc.append(int(l.split()[2]))
 
 def step(inp, z, A, B, C):
     if inp == z % 26 + B:
@@ -22,13 +18,13 @@ def step(inp, z, A, B, C):
 def solve(order, l=[], z=0):
     if len(l) == 14 and z == 0:
         return ''.join(map(str, l))
-    A, B, C = abc[len(l)]
+    A, B, C = abc[len(l) * 3:][:3]
     tries = order
     if A == 26:
         w = z % 26 + B
         if w < 1 or w > 9:
-            return None # No way this can lead to z == 0
-        tries = [w]
+            return
+        tries = [w] # Only this value can lead to z == 0
     for w in tries:
         if ok := solve(order, l + [w], step(w, z, A, B, C)):
             return ok
