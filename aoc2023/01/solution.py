968a433eca94fb3027a79bf789887a056d2a990f
Sam Hocevar <sam@hocevar.net>
2023-12-01 18:38:03 +0100

    Remove a for loop

--- aoc2023/01/solution.py
+++ aoc2023/01/solution.py
@@ -5,17 +5,16 @@ sum1, sum2 = 0, 0
 with open('input.txt') as f:
     for line in f:
 
+        # Part 1: just convert all digit characters to integers and take the first and last elements
         if digits := list(map(int, filter(str.isdigit, line))):
             sum1 += 10 * digits[0] + digits[-1]
 
+        # If a number (word or single digit) is found at pos, return its integer value, otherwise None
         def get_digit(pos):
-            lut = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-            for n, k in enumerate(lut):
-                if line[pos:].startswith(k):
-                    return n + 1
-            if line[pos].isdigit():
-                return int(line[pos])
+            lut = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', *map(str, range(1, 10))]
+            return next((n % 9 + 1 for n, k in enumerate(lut) if line[pos:].startswith(k)), None)
 
+        # Part 2: same but use a helper function that also understands words
         if digits := list(filter(None, map(get_digit, range(len(line))))):
             sum2 += 10 * digits[0] + digits[-1]
 
