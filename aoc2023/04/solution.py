8f00dd8945c4cbca8e96a6581ef45006fa533cc5
Sam Hocevar <sam@hocevar.net>
2023-12-04 14:12:06 +0100

    Minor tweak

--- aoc2023/04/solution.py
+++ aoc2023/04/solution.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-from operator import and_
 from re import findall
 
-# Compute the number of wins for each card using set intersection
+# Compute the number of wins for each card, using simple set intersection
 with open('input.txt') as f:
-    wins = [len(and_(*map(lambda s: set(map(int, s.split())), findall(r'\d+ +[\d ]*', l)))) for l in f]
+    wins = [len(set.intersection(*({int(n) for n in s.split()} for s in findall(r'\d+ [\d ]*', l)))) for l in f]
 
 # Part 1: sum of all scores
 print(sum(int(2 ** (w - 1)) for w in wins))
 
-# Part 2: accumulate all numbers of copies and compute their sum
+# Part 2: accumulate all copy counts and compute their sum
 copies = [1] * len(wins)
 for n, w in enumerate(wins):
     for k in range(w):
