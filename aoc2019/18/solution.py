d0d85fefba75f01422cdd46d82a408520b9093b0
Sam Hocevar <sam@hocevar.net>
2023-12-10 16:20:53 +0100

    Rework pathfinding logic to precompute most of the data (60× speed increase)

--- aoc2019/18/solution.py
+++ aoc2019/18/solution.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python
 
+from re import finditer
 from heapq import heappush, heappop
 
 with open('input.txt') as f:
     data = f.readlines()
-start = next((l.index('@'), n, ()) for n, l in enumerate(data) if '@' in l)
+distances, tokens = {}, {m[0]: (m.start(0), n) for n, l in enumerate(data) for m in finditer(r'[@\w]', l)}
 
-# Low-level pathfinding function: list all reachable keys from a given
-# position on the map and a list of collected keys. An already opened door
-# or an already collected key is considered an empty cell. We use a heap
-# for the frontier to make sure we always get the shortest path.
-def reachable(start):
-    empty = {'.', '@'}.union(start[2]).union(c.upper() for c in start[2])
-    todo, done = [(0, *start[:2])], set()
+# Compute the distances between all special tokens
+for token, pos in tokens.items():
+    distances[token], todo, done = {}, [(0, *pos)], set()
     while todo:
-        dist, x, y = heappop(todo)
+        cost, x, y = heappop(todo)
         if (x, y) not in done:
             done.add((x, y))
             match data[y][x]:
-                case c if c in empty:
+                case c if c in ['.', token]:
                     for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-                        heappush(todo, (dist + 1, x + dx, y + dy))
-                case c if c >= 'a' and c <= 'z':
-                    yield (dist, x, y, c)
+                        heappush(todo, (cost + 1, x + dx, y + dy))
+                case c if c in tokens:
+                    distances[token][c] = cost
 
-# Part 1: use high-level pathfinding to explore the map in large steps,
-# jumping from one reachable key to another. Stop when all keys have been
-# collected.
-todo, done = [(0, start)], set()
-while todo:
-    cost, state = heappop(todo)
-    if state not in done:
-        done.add(state)
-        targets = list(reachable(state))
-        if not targets:
-            print(cost)
-            break
-        for dist, x, y, c in targets:
-            heappush(todo, (cost + dist, (x, y, tuple(sorted({c}.union(state[2]))))))
+# Use high-level pathfinding to explore the map in large steps, going from
+# tokens to tokens instead of caring about the exact path.
+def visit(start):
+    todo, done, target = [(0, start, '')], set(), sum(1 for c in tokens if c.islower())
+    while todo:
+        cost, pos, keys = heappop(todo)
+        # Stop when all keys have been collected
+        if len(keys) == target:
+            return cost
+        if (pos, keys) not in done:
+            done.add((pos, keys))
+            for t, dist in distances[pos].items():
+                if not t.isupper() or t.lower() in keys:
+                    heappush(todo, (cost + dist, t, ''.join(sorted(keys + t)) if t.islower() and t not in keys else keys))
+
+# Part 1: visit the map using one agent
+print(visit('@'))
